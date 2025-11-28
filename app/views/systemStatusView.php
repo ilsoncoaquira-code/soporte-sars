e315179d866b0c0afcdd1f3dc4ac98a13cd03afe
ilsoncoaquira-code <ilson.coaquira@gmail.com>
2025-11-28 15:10:16 -0500

    Subir archivos actuales del proyecto Visual Studio

---
+++ app/views/systemStatusView.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Estado del Sistema - Soporte Sars</title>
+</head>
+<body>
+
+    <h1>Estado del Sistema – Soporte Sars</h1>
+
+    <p>Servicio: <?= $data["service"] ?></p>
+    <p>Base de datos: <?= $data["database"] ?></p>
+    <p>Estado general: <?= $data["status"] ?></p>
+
+</body>
+</html>
